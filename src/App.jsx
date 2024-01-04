8563082949bd5ec4adf2060ae153f560dd251a89
Idris <idbalorg@gmail.com>
2024-01-04 17:05:30 +0100

    skelenton

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import Product from './pages/Product'
+import Pricing from './pages/Pricing'
+import PageNotFound from './pages/PageNotFound'
+import AppLayout from './pages/AppLayout'
 
 function App() {
   return (
-    <div>App</div>
+    
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='product' element={<Product/>}/>
+        <Route path='pricing' element={<Pricing/>}/>
+        <Route path='*' element={<PageNotFound/>} />
+        <Route path='app' element={<AppLayout/>}/>
+      </Routes>
+    
+    </BrowserRouter>
+    
   )
 }
 
